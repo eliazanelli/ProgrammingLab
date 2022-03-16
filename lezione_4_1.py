1b87d3a34f4b0dad1874de1643becf241cffb11a
Elia Zanelli <101268755+eliazanelli@users.noreply.github.com>
2022-03-16 12:01:26 +0000

    Non ricordo

---
+++ lezione_4_1.py
@@ -0,0 +1,21 @@
+class CSVFile():
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_data(self):
+        lista = []
+        file = open(self.name, 'r')
+        for line in file:
+            elements = line.split(',')
+            if elements[0].startswith('Date'):
+                pass
+            else:
+                elements[-1] = elements[-1].strip()
+                # elements[-1] = elements[-1][0:-1]
+                lista.append(elements)
+        return lista
+
+# File = CSVFile('shampoo_sales.csv')
+# print(File.get_data())
+            
