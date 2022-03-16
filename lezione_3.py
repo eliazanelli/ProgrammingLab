ec97d708b59c1e3a190171f01e48187f04c7fc64
Elia Zanelli <101268755+eliazanelli@users.noreply.github.com>
2022-03-16 09:53:47 +0000

    Aggiunta controllo se il file è vuoto

--- lezione_3.py
+++ lezione_3.py
@@ -1,11 +1,14 @@
 file = open('shampoo_sales.csv', 'r')
 
 def sum_csv(file_name):
-    sum_value = 0
-    for line in file_name:
-        elements = line.split(',')
-        if elements[0] != 'Date':
-            value = float(elements[1])
-            sum_value += value
+    if len(file_name.readline()) != 0:
+        sum_value = 0
+        for line in file_name:
+            elements = line.split(',')
+            if elements[0] != 'Date':
+                value = float(elements[1])
+                sum_value += value
+    else:
+        print("Il file è vuoto")
     return sum_value
 print("The sum of shampoo sales over yers is: {}".format(sum_csv(file)))
