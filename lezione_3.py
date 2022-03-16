180341ede364f109c7ff9cf5eb5f94e16c00bb0f
Elia Zanelli <101268755+eliazanelli@users.noreply.github.com>
2022-03-16 10:02:52 +0000

    Inserito File_name

--- lezione_3.py
+++ lezione_3.py
@@ -1,14 +1,16 @@
-file = open('shampoo_sales.csv', 'r')
+# from lezione_2 import sum_list
 
 def sum_csv(file_name):
-    if len(file_name.readline()) != 0:
+    file = open(file_name, 'r')
+    if len(file.readline()) != 0:
         sum_value = 0
-        for line in file_name:
+        for line in file:
             elements = line.split(',')
             if elements[0] != 'Date':
                 value = float(elements[1])
                 sum_value += value
     else:
         print("Il file è vuoto")
+    file.close()
     return sum_value
-print("The sum of shampoo sales over yers is: {}".format(sum_csv(file)))
+print("The sum of shampoo sales over yers is: {}".format(sum_csv('shampoo_sales.csv')))
